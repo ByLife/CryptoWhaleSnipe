903d54ea7f4849f1eb8c684eb815965a941719a2
bylife <bylife@bylife.fr>
2025-01-17 01:01:19 +0100

    Change the time fetch to 3h ago instead of 3days

--- src/autoload/loader.autoload.ts
+++ src/autoload/loader.autoload.ts
@@ -300,11 +300,7 @@ export class Autoload { // This is the class that starts the server
           const currentTime = new Date();
           // Last 3 hours:
           const threeHoursAgo = Math.floor(
-            (currentTime.getTime() - 3 * 24 * 60 * 60 * 1000) / 1000
-          );
-
-          const threeDaysAgo = Math.floor(
-            (currentTime.getTime() - 3 * 24 * 60 * 60 * 1000) / 1000
+            (currentTime.getTime() - 3 * 60 * 60 * 1000) / 1000
           );
   
           for (const wallet of wallets) {
